0b7d7a7f2f55fb6113a1210ce2ad9437d16115cf
rzarajczyk <rzarajczyk@gmail.com>
2024-07-24 22:25:06 +0200

    13

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 mkdir -p ~/Library/family-rules-client
 cd ~/Library/family-rules-client
-git clone https://github.com/rzarajczyk/family-rules-client.git
+git clone https://github.com/rzarajczyk/family-rules-client.git .
 
-cd scripts
 envsubst < pl.zarajczyk.family-rules-client.plist > ~/Library/LaunchAgents/pl.zarajczyk.family-rules-client.plist

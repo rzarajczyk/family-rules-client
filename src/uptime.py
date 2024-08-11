a5d2ae2d0d8c95871c306378221b47eea65be55c
rzarajczyk <rzarajczyk@gmail.com>
2024-08-11 11:14:42 +0200

    40

--- src/uptime.py
+++ src/uptime.py
@@ -28,19 +28,49 @@ class PsUptime(Uptime):
 class AppleScreenTimeUptime(Uptime):
     # https://felixkohlhas.com/projects/screentime/
     def get(self) -> UsageUpdate:
-        usage = self.__query_database(datetime.date.today())
-        sum = self.__sum_usage_seconds_per_app(usage)
+        today = datetime.date.today()
+        knowledge_db = expanduser("~/Library/Application Support/Knowledge/knowledgeC.db")
+        with sqlite3.connect(knowledge_db) as con:
+            con.row_factory = sqlite3.Row
+            return UsageUpdate(
+                screen_time=self.__query_for_screen_time(con, today),
+                applications=self.__get_app_usages(con, today),
+                absolute=True
+            )
+
+    def __query_for_screen_time(self, con, date: datetime.date):
+        cur = con.cursor()
+        date_from = self.__date_to_unix_timestamp_utc(date)
+        date_to = self.__date_to_unix_timestamp_utc(date + datetime.timedelta(days=1))
+        query = f"""
+            SELECT
+                sum(ZOBJECT.ZENDDATE - ZOBJECT.ZSTARTDATE) AS "usage_seconds"
+            FROM
+                ZOBJECT 
+            WHERE
+                ZSTREAMNAME = "/display/isBacklit"
+                AND
+                (ZOBJECT.ZSTARTDATE + 978307200) > {date_from}
+                AND
+                (ZOBJECT.ZENDDATE + 978307200) < {date_to}
+            """
+        cur.execute(query)
+
+        # Fetch all rows from the result set
+        rows = cur.fetchall()
+        results = [dict(row) for row in rows]
+        return timedelta(seconds=int(results[0]['usage_seconds']))
+
+    def __get_app_usages(self, con, date: datetime.date):
+        app_usage_reports = self.__query_for_app_usage(con, date)
+        sum = self.__sum_usage_seconds_per_app(app_usage_reports)
         sum = dict(reversed(sorted(sum.items(), key=lambda x: x[1])))
         applications = {}
         for app in sum:
             duration = datetime.timedelta(seconds=sum[app])
             name = app
             applications[name] = duration
-        return UsageUpdate(
-            screen_time=timedelta(seconds=0),
-            applications=applications,
-            absolute=True
-        )
+        return applications
 
     def __date_to_unix_timestamp_utc(self, date_obj):
         # Convert the date object to a datetime object
@@ -57,57 +87,53 @@ class AppleScreenTimeUptime(Uptime):
     def __from_utc_timestamp(self, t):
         return datetime.datetime.fromtimestamp(t, datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
 
-    def __query_database(self, date: datetime.date):
-        # Connect to the SQLite database
-        knowledge_db = expanduser("~/Library/Application Support/Knowledge/knowledgeC.db")
-        with sqlite3.connect(knowledge_db) as con:
-            con.row_factory = sqlite3.Row
-            cur = con.cursor()
+    def __query_for_app_usage(self, con, date: datetime.date):
+        cur = con.cursor()
 
-            # Execute the SQL query to fetch data
-            # Modified from https://rud.is/b/2019/10/28/spelunking-macos-screentime-app-usage-with-r/
-            date_from = self.__date_to_unix_timestamp_utc(date)
-            date_to = self.__date_to_unix_timestamp_utc(date + datetime.timedelta(days=1))
-            query = f"""
-            SELECT
-                ZOBJECT.ZVALUESTRING AS "app", 
-                (ZOBJECT.ZENDDATE - ZOBJECT.ZSTARTDATE) AS "usage_seconds",
-                (ZOBJECT.ZSTARTDATE + 978307200) as "start_time", 
-                (ZOBJECT.ZENDDATE + 978307200) as "end_time",
-                (ZOBJECT.ZCREATIONDATE + 978307200) as "created_at", 
-                ZOBJECT.ZSECONDSFROMGMT AS "tz",
-                ZSOURCE.ZDEVICEID AS "device_id",
-                ZMODEL AS "device_model"
-            FROM
-                ZOBJECT 
-                LEFT JOIN
-                ZSTRUCTUREDMETADATA 
-                ON ZOBJECT.ZSTRUCTUREDMETADATA = ZSTRUCTUREDMETADATA.Z_PK 
-                LEFT JOIN
-                ZSOURCE 
-                ON ZOBJECT.ZSOURCE = ZSOURCE.Z_PK 
-                LEFT JOIN
-                ZSYNCPEER
-                ON ZSOURCE.ZDEVICEID = ZSYNCPEER.ZDEVICEID
-            WHERE
-                ZSTREAMNAME = "/app/usage"
-                AND
-                start_time > {date_from}
-                AND
-                end_time < {date_to}
-            ORDER BY
-                ZSTARTDATE DESC
-            """
-            cur.execute(query)
+        # Execute the SQL query to fetch data
+        # Modified from https://rud.is/b/2019/10/28/spelunking-macos-screentime-app-usage-with-r/
+        date_from = self.__date_to_unix_timestamp_utc(date)
+        date_to = self.__date_to_unix_timestamp_utc(date + datetime.timedelta(days=1))
+        query = f"""
+        SELECT
+            ZOBJECT.ZVALUESTRING AS "app", 
+            (ZOBJECT.ZENDDATE - ZOBJECT.ZSTARTDATE) AS "usage_seconds",
+            (ZOBJECT.ZSTARTDATE + 978307200) as "start_time", 
+            (ZOBJECT.ZENDDATE + 978307200) as "end_time",
+            (ZOBJECT.ZCREATIONDATE + 978307200) as "created_at", 
+            ZOBJECT.ZSECONDSFROMGMT AS "tz",
+            ZSOURCE.ZDEVICEID AS "device_id",
+            ZMODEL AS "device_model"
+        FROM
+            ZOBJECT 
+            LEFT JOIN
+            ZSTRUCTUREDMETADATA 
+            ON ZOBJECT.ZSTRUCTUREDMETADATA = ZSTRUCTUREDMETADATA.Z_PK 
+            LEFT JOIN
+            ZSOURCE 
+            ON ZOBJECT.ZSOURCE = ZSOURCE.Z_PK 
+            LEFT JOIN
+            ZSYNCPEER
+            ON ZSOURCE.ZDEVICEID = ZSYNCPEER.ZDEVICEID
+        WHERE
+            ZSTREAMNAME = "/app/usage"
+            AND
+            start_time > {date_from}
+            AND
+            end_time < {date_to}
+        ORDER BY
+            ZSTARTDATE DESC
+        """
+        cur.execute(query)
 
-            # Fetch all rows from the result set
-            rows = cur.fetchall()
-            results = [dict(row) for row in rows]
-            for result in results:
-                result['start_time'] = self.__from_utc_timestamp(result['start_time'])
-                result['end_time'] = self.__from_utc_timestamp(result['end_time'])
-                result['created_at'] = self.__from_utc_timestamp(result['created_at'])
-            return results
+        # Fetch all rows from the result set
+        rows = cur.fetchall()
+        results = [dict(row) for row in rows]
+        for result in results:
+            result['start_time'] = self.__from_utc_timestamp(result['start_time'])
+            result['end_time'] = self.__from_utc_timestamp(result['end_time'])
+            result['created_at'] = self.__from_utc_timestamp(result['created_at'])
+        return results
 
     def __sum_usage_seconds_per_app(self, data):
         usage_dict = {}

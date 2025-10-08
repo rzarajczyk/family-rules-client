695de2b2b958a07415f342d7813a84747c991804
rzarajczyk <rzarajczyk@gmail.com>
2025-10-08 18:07:38 +0200

    code signing

--- build-mac-app.sh
+++ build-mac-app.sh
@@ -5,9 +5,3 @@ rm -r dist
 
 echo "Creating app..."
 pyinstaller --log-level=WARN app-macos.spec
-
-echo "Ad hoc code signing the app..."
-codesign --force --deep --sign - dist/FamilyRules.app
-
-echo "Verifying signature..."
-codesign --verify --verbose dist/FamilyRules.app

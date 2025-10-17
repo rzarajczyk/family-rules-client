d7e33de5d25b0366af0a99b11cd10b4f20153ab3
rzarajczyk <rzarajczyk@gmail.com>
2025-10-17 22:19:19 +0200

    bugfix

--- tests/test_image.py
+++ tests/test_image.py
@@ -1,13 +1,19 @@
 import pytest
 
 def test_image_manipulation_works():
+    import tempfile
+    import os
     from PilLite import Image
+
     img = Image.open("tests/resources/icon.png")
     img = img.resize((64, 64))
-    img.save("/tmp/output.png", 'png')
 
-    img = Image.open("/tmp/output.png")
-    w, h = img.size
+    with tempfile.TemporaryDirectory() as tmpdir:
+        out_path = os.path.join(tmpdir, "output.png")
+        img.save(out_path, 'png')
 
-    assert w == 64
-    assert h == 64
+        img = Image.open(out_path)
+        w, h = img.size
+
+        assert w == 64
+        assert h == 64
